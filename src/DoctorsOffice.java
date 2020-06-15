ae79652b38a0242014e4c0d07c970f4576963e4b
joycejosephtwenty20 <joyce.j106s13@gmail.com>
2020-06-15 16:54:17 -0400

    DoctorsOfficeExample

---
+++ src/DoctorsOffice.java
@@ -0,0 +1,15 @@
+public class DoctorsOffice {
+    public static void main(String[] args) {
+        String patientName = "Stacey Manson";
+        boolean hasInsurance = false;
+        int temp = 102;
+        double numVisits = 5.02;
+
+        System.out.println("Patient Details:");
+        System.out.println("Name: " + patientName);
+        System.out.println("Patient has insurance: " + hasInsurance);
+        System.out.println("Body temprature: " + temp);
+        System.out.println("Number of visits: " + numVisits);
+        }
+
+    }
